7b2e2433b5d76be9b98cf8acbc8a6d0ca2143dc6
Jim Lee <jimleee945@gmail.com>
2019-05-27 16:27:03 -0700

    Only working in dev

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,46 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const express = require("express");
+const axios = require("axios");
+const h2p = require("html2plaintext");
+
+// eslint-disable-next-line no-useless-escape
+const URL_REGEX = /^(https?:\/\/)?([\w.-]+(?:\.[\w\.-]+)+[\w\-\._~\/?#[\]@!\$&'\(\)\*\+,;=.]+)$/;
+
+const app = express();
+app.get("/", (request, response) => {
+    response.send("Hello World");
+})
+app.get("/api", (request, response) => {
+    const urlQuery = request.query.url;
+    let url;
+
+    if (urlQuery) {
+        const [fullMatch, group1, group2] = urlQuery.match(URL_REGEX);
+        if (group2) {
+            url = "https:\\\\" + group2;
+        }
+    }
+
+    return axios.get(url).then(resp => {
+        let data = h2p(resp.data);
+        let count = {};
+
+        data.match(/\w+/g).forEach(word => {
+            if (count.hasOwnProperty(word)) {
+                count[word]++;
+            } else {
+                count[word] = 1;
+            }
+        })
+        return response.status(200).send(count);
+    }).catch(error => {
+        if (error.response) {
+            return response.status(error.response.status).send(error.response.data);
+        }
+        return response.sendStatus(500);
+    });
+});
+exports.api = functions.https.onRequest(app);
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,96 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <hello-world msg="Welcome to Your Vue.js App"></hello-world>
-    </div>
+    <b-container id="id">
+        <b-card>
+            <b-form-group label="URL"
+                          label-for="url-input"
+                          v-bind:state="urlState"
+                          v-bind:invalid-feedback="invalidFeedBack">
+                <b-input-group>
+                    <b-form-input id="url-input" v-model="url" type="url" v-bind:state="urlState" v-on:keyup.enter.native="getCount"></b-form-input>
+
+                    <b-btn slot="append" v-on:click.stop="getCount">
+                        Test
+                    </b-btn>
+                </b-input-group>
+            </b-form-group>
+            
+            <b-list-group>
+                <b-list-group-item v-for="word in Object.keys(wordCount)" v-bind:key="word" 
+                class="d-flex justify-content-between align-items-center">
+                    {{ word }}
+                    <b-badge variant="primary">
+                        {{ wordCount[word] }}
+                    </b-badge>
+                </b-list-group-item>
+            </b-list-group>
+        </b-card>
+    </b-container>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+
+const http = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+});
+
+const URL_REGEX = /^(https?:\/\/)?([\w.-]+(?:\.[\w\.-]+)+[\w\-\._~\/?#[\]@!\$&'\(\)\*\+,;=.]+)$/;
 
 export default {
     name: "App",
+    data() {
+        return {
+            url: "",
+            errorMsg: "",
+            wordCount: {},
+        };
+    },
+    computed: {
+        isUrl() {
+            if (this.url.length) {
+                return URL_REGEX.test(this.url);
+            }
+            return false;
+        },
+        urlState() {
+            if (this.errorMsg) {
+                return false;
+            }
+            if (this.url.length) {
+                return this.isUrl;
+            }
+            return null;
+        },
+        invalidFeedBack() {
+            if (this.errorMsg) {
+                return this.errorMsg;
+            }
+            return "Invalid URL";
+        },
+    },
+    methods: {
+        async getCount() {
+            if (!this.isUrl) {
+                return;
+            }
+
+            this.errorMsg = "";
+            try {
+                const resp = await http.get("api", {
+                    params: {
+                        url: this.url
+                    }
+                }); 
+                this.wordCount = resp.data;
+
+            } catch(e) {
+                console.log(e);
+                if (e.response) {
+                    this.errorMsg = `${e.response.status} ${e.response.data}`;
+                }
+            }
+        }
+    }
 };
 </script>
 
